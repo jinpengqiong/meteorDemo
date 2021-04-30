252a7c98755864daa332ab5251bbcda80e3350ae
Peng.Jin <peng.jin@symbio.com>
2021-04-30 16:42:34 +0800

    ok

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { LinkCollection } from '../db/link';
@@ -6,23 +6,28 @@ import { LinkCollection } from '../db/link';
 export const Hello = () => {
 
   useTracker(() => {
+    window.addEventListener('onchange', function (params) {
+      console.log('params :>> ', params);
+    });
     Meteor.subscribe('link');
     return LinkCollection.find().fetch();
   })
   const [counter, setCounter] = useState(0);
-
+  const inputRef = useRef();
   const increment = () => {
-    setCounter(counter + 1);
-    Meteor.call('lists.insert', {
-      title: counter + 1,
-      url: 'https://github.com/jinpengqiong',
-    });
-    Meteor.call('link.insert', counter + 1);
+    console.log('inputRef :>> ', inputRef.current);
+    // setCounter(counter + 1);
+    // Meteor.call('lists.insert', {
+    //   title: counter + 1,
+    //   url: 'https://github.com/jinpengqiong',
+    // });
+    // Meteor.call('link.insert', counter + 1);
   };
 
   return (
     <div>
-      <button onClick={increment}>Click Me</button>
+      <input type="text" ref={inputRef} />
+      <button onClick={increment}>Submit</button>
       <p>You've pressed the button {counter} times.</p>
     </div>
   );
