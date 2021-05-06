5454dc557c80be05bac40210f3adf7543d1fbe2d
Peng.Jin <peng.jin@symbio.com>
2021-05-06 09:19:35 +0800

    ok

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,21 +1,23 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { LinkCollection } from '../db/link';
 
 export const Hello = () => {
-
+  let search = '';
   useTracker(() => {
-    window.addEventListener('onchange', function (params) {
-      console.log('params :>> ', params);
-    });
     Meteor.subscribe('link');
     return LinkCollection.find().fetch();
   })
+  useEffect(() => {
+    console.log('window.location.search :>> ', window.location.search);
+    search = window.location.search;
+  }, [])
   const [counter, setCounter] = useState(0);
-  const inputRef = useRef();
   const increment = () => {
-    console.log('inputRef :>> ', inputRef.current);
+    form2.submit();
+    console.log('search :>> ', search);
+    // console.log('location :>> ', location);
     // setCounter(counter + 1);
     // Meteor.call('lists.insert', {
     //   title: counter + 1,
@@ -23,11 +25,16 @@ export const Hello = () => {
     // });
     // Meteor.call('link.insert', counter + 1);
   };
-
   return (
     <div>
-      <input type="text" ref={inputRef} />
-      <button onClick={increment}>Submit</button>
+      <form name="form2" action="/">
+        <label>Name:</label>
+        <input type="text" id="fname" name="content" />
+        <br/>
+        <br/>
+        <input type="button" onClick={increment} value="Submit" />
+      </form>
+      {/* <button onClick={increment}>Submit</button> */}
       <p>You've pressed the button {counter} times.</p>
     </div>
   );
