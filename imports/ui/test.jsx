b3a025ccdc2d698dfaa38d5c82f9f965ccd0d93a
Peng.Jin <peng.jin@symbio.com>
2021-05-08 21:53:53 +0800

    ok

--- imports/ui/test.jsx
+++ imports/ui/test.jsx
@@ -1,60 +1,39 @@
-import React, { useState } from 'react';
-import './index.css';
+import React,{useState,memo,useMemo,useCallback} from 'react';
 
-export const TEST = () => {
-  function Child1(porps) {
-  console.log('child', porps);
-  const { num, handleClick } = porps;
-  return (
-    <div
-      onClick={() => {
-        handleClick(num + 1);
-      }}
-    >
-      child
-    </div>
-  );
+function SubCounter({onClick,data}){
+    console.log('SubCounter render', onClick, data);
+    return (
+        <button onClick={onClick}>{data.number}</button>
+    )
+}
+SubCounter = memo(SubCounter);
+
+let oldData,oldAddClick;
+export function TEST(){
+    console.log('Counter render');
+    const [name,setName]= useState('计数器');
+    const [number,setNumber] = useState(0);
+    // 父组件更新时，这里的变量和函数每次都会重新创建，那么子组件接受到的属性每次都会认为是新的
+    // 所以子组件也会随之更新，这时候可以用到 useMemo
+    // 有没有后面的依赖项数组很重要，否则还是会重新渲染
+    // 如果后面的依赖项数组没有值的话，即使父组件的 number 值改变了，子组件也不会去更新
+    // const data = useMemo(()=>({number}),[]);
+    const data = useMemo(() => ({ number }), [number]);
+    console.log('data :>> ', data);
+    console.log('data===oldData ',data===oldData);
+    oldData = data;
+
+    // 有没有后面的依赖项数组很重要，否则还是会重新渲染
+    const addClick = useCallback(()=>{
+        setNumber(number+1);
+    },[name]);
+    console.log('addClick===oldAddClick ',addClick===oldAddClick);
+    oldAddClick=addClick;
+    return (
+        <>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+            <SubCounter data={data} onClick={addClick}/>
+        </>
+    )
 }
 
-function Child2(porps) {
-  console.log('Child2', porps);
-  const { text, handleClick } = porps;
-  return (
-    <div>
-      child2
-      <Grandson text={text} handleClick={handleClick} />
-    </div>
-  );
-}
-
-function Grandson(porps) {
-  console.log('Grandson', porps);
-  const { text, handleClick } = porps;
-  return (
-    <div
-      onClick={() => {
-        handleClick(text + 1);
-      }}
-    >
-      grandson
-    </div>
-  );
-}
-
-function Parent() {
-  let [num, setNum] = useState(0);
-  let [text, setText] = useState(1);
-
-  return (
-    <div>
-      <Child1 num={num} handleClick={setNum} />
-      <Child2 text={text} handleClick={setText} />
-    </div>
-  );
-}
-  return (
-    <div>
-      <Parent />
-    </div>
-  );
-};
